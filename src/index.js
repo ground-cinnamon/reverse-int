eddc51b8208bef4b777af30977c3dc7eb12a5121
ground-cinnamon <morkovog@gmail.com>
2024-09-28 13:07:03 +0300

    feat: make the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = -n;
+    }
+
+    let string = n.toString();
+
+    if (string[string.length - 1] === "0") {
+        string = string.slice(0, string.length - 1);
+    }
+
+    return string.split("").reverse().join("");
+};
